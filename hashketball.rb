e80a71963f3208c07fd6f5546f043609c8562ca3
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-07 16:37:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,5 +130,5 @@ def num_points_scored(player_name, game_hash)
  game_hash.each do |team, team_hash|
    team_hash[:players].each do |player, player_hash|
      if player_hash == player_name 
-       return player
+       return player[:name]
 end 
